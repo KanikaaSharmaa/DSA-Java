2dd0064e5175ecc2fe0a910e0c1d47c0c1d266bd
KanikaaSharmaa <157880822+KanikaaSharmaa@users.noreply.github.com>
2024-09-27 22:29:32 +0530

    Created bar chart
    This Java program creates a simple bar chart based on user input. First, it prompts the user to enter the size of an array and then the elements of that array. It identifies the maximum value in the array to determine the height of the bars in the chart. The program then iterates from the maximum value down to 1, printing asterisks (*) for each element that meets or exceeds the current height being represented. If an element is less than the current height being printed, it outputs a tab space instead. The result is a vertical bar chart where each element's value is represented by a column of asterisks.

---
+++ Trick Questions/Bar_Chart.java
@@ -0,0 +1,76 @@
+/*
+    Output Format
+    A bar chart of asteriks representing value of array a
+
+    Constraints
+    1<=n<=30
+    0<=n1,n2,....n elements <=10
+
+    Sample Input
+    8
+    8
+    2
+    5
+    7
+    5
+    1
+    6
+    9
+
+    Sample Output
+                                *
+    *                           *
+    *           *               *
+    *           *           *   *
+    *       *   *   *       *   *
+    *       *   *   *       *   *
+    *       *   *   *       *   *
+    *   *   *   *   *       *   *
+    *   *   *   *   *   *   *   *
+    
+ */
+
+import java.util.*;
+public class Bar_Chart 
+{
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int n = scn.nextInt();
+        System.out.println();
+
+        int []array = new int[n];
+        System.out.print("Enter the elements of an array: ");
+        for(int i=0;i<array.length;i++)
+        {
+            array[i] = scn.nextInt();
+        }
+        System.out.println();
+
+        int max = array[0];
+        for(int i=0;i<array.length;i++)
+        {
+            if(array[i]>max)
+            {
+                max = array[i];  
+            }
+        }
+        for(int i=max;i>0;i--)
+        {
+            for(int j=0;j<array.length;j++)
+            {
+                if(array[j]>=i)
+                {
+                    System.out.print("*\t");
+                }
+                else
+                {
+                    System.out.print("\t");
+                }
+            }
+            System.out.println();
+        }
+        scn.close();
+    }
+    
+}
